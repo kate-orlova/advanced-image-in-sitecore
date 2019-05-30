52c6c610fdaad87d8a6a7a6682695b3bef479be4
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-30 18:49:59 +0100

    Declared an image cropping method

--- src/Foundation/AdvancedImage/Processors/CropProcessor.cs
+++ src/Foundation/AdvancedImage/Processors/CropProcessor.cs
@@ -1,7 +1,17 @@
-﻿namespace AdvancedImage.Processors
+﻿using Sitecore.Data.Items;
+using System.IO;
+
+namespace AdvancedImage.Processors
 {
     public class CropProcessor
     {
         private static readonly string[] IMAGE_EXTENSIONS = { "bmp", "jpeg", "jpg", "png", "gif" };
+
+        private Stream GetCroppedImage(string extension, int width, int height, float cx, float cy, MediaItem mediaItem)
+        {
+            var outputStrm = new MemoryStream();
+
+            return outputStrm;
+        }
     }
 }
