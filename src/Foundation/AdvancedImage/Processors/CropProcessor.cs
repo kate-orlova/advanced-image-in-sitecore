3fe8830c380e7d99d25d18573609bc8612c3c11b
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-31 08:59:16 +0100

    Implemented GetCroppedImage()

--- src/Foundation/AdvancedImage/Processors/CropProcessor.cs
+++ src/Foundation/AdvancedImage/Processors/CropProcessor.cs
@@ -1,5 +1,7 @@
-﻿using Sitecore.Data.Items;
+﻿using System.Drawing;
+using Sitecore.Data.Items;
 using System.IO;
+using ImageProcessor.Imaging;
 
 namespace AdvancedImage.Processors
 {
@@ -10,6 +12,14 @@ namespace AdvancedImage.Processors
         private Stream GetCroppedImage(string extension, int width, int height, float cx, float cy, MediaItem mediaItem)
         {
             var outputStrm = new MemoryStream();
+            var mediaStrm = mediaItem.GetMediaStream();
+            var img = Image.FromStream(mediaStrm);
+            var proc = new ImageProcessor.ImageFactory();
+            proc.Load(img);
+
+            var axis = new float[] { cy, cx };
+            proc = proc.Resize(new ResizeLayer(new Size(width, height), ResizeMode.Crop, AnchorPosition.Center, true, centerCoordinates: axis));
+            proc.Save(outputStrm);
 
             return outputStrm;
         }
