0101ae44d311566ed2a9cb29929fa590baf3e449
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-03 18:35:47 +0100

    Implemented AdvancedImageEditorDetailsModel model

---
+++ src/Foundation/AdvancedImage/Fields/Editor/AdvancedImageEditorDetailsModel.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Shell.Applications.ContentEditor;
+
+namespace AdvancedImage.Fields.Editor
+{
+    public class AdvancedImageEditorDetailsModel
+    {
+        public XmlValue XmlValue { get; set; }
+
+        public MediaItem MediaItem { get; set; }
+
+        public Item InnerItem => this.MediaItem?.InnerItem;
+
+        public string ImageWidth => this.XmlValue?.GetAttribute("width");
+
+        public string ImageHeight => this.XmlValue?.GetAttribute("height");
+
+        public string OriginalDimensions => this.InnerItem?["Dimensions"];
+
+        public string OriginalAlternateText => this.InnerItem?["Alt"];
+
+        public string UpdatedAlternateText => this.XmlValue?.GetAttribute("alt");
+    }
+}
