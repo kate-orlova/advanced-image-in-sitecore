2ca02a2bd1d36eab1364c5a589b25c544ec7a04a
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-03 18:19:50 +0100

    Implemented Start()

--- src/Foundation/AdvancedImage/App_Start/RazorGeneratorMvcStart.cs
+++ src/Foundation/AdvancedImage/App_Start/RazorGeneratorMvcStart.cs
@@ -1,6 +1,24 @@
-﻿namespace AdvancedImage.App_Start
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using RazorGenerator.Mvc;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(AdvancedImage.App_Start.RazorGeneratorMvcStart), "Start")]
+namespace AdvancedImage.App_Start
 {
     public class RazorGeneratorMvcStart
     {
+        public static void Start()
+        {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly)
+            {
+                UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
+            };
+
+            ViewEngines.Engines.Insert(0, engine);
+
+            // StartPage lookups are done by WebPages. 
+            VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
+        }
     }
 }
