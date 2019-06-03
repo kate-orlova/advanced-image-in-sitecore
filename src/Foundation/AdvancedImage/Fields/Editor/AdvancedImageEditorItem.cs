65be73d63252b9175037b1b49b28ad56d6434265
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-03 18:34:08 +0100

    Implemented AdvancedImageEditorItem model

---
+++ src/Foundation/AdvancedImage/Fields/Editor/AdvancedImageEditorItem.cs
@@ -0,0 +1,17 @@
+﻿namespace AdvancedImage.Fields.Editor
+{
+    public class AdvancedImageEditorItem
+    {
+        public string ControlId { get; set; }
+
+        public string ImageSrc { get; set; }
+
+        public string ImageId { get; set; }
+
+        public string ImageAlt { get; set; }
+
+        public string CropFocus { get; set; }
+
+        public bool ShowFull { get; set; }
+    }
+}
