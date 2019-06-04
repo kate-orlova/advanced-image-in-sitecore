5caa933cc8a1bd85db7328c91d0900d5f25e9878
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-04 21:57:30 +0100

    Implemented AdvancedImageEditorModel model

---
+++ src/Foundation/AdvancedImage/Fields/Editor/AdvancedImageEditorModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AdvancedImage.Fields.Editor
+{
+    public class AdvancedImageEditorModel
+    {
+        public string ControlId { get; set; }
+
+        public AdvancedImageEditorThumbnailsModel Thumbnails { get; set; }
+
+        public string IsDebug { get; set; }
+
+        public AdvancedImageEditorItem Image { get; set; }
+
+        public AdvancedImageEditorDetailsModel Details { get; set; }
+    }
+}
