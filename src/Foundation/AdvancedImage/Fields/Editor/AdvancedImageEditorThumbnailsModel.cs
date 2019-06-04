bdb821acf2d5fd30f1533df2b390578e5126b362
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-04 21:52:50 +0100

    Implemented AdvancedImageEditorThumbnailsModel model

---
+++ src/Foundation/AdvancedImage/Fields/Editor/AdvancedImageEditorThumbnailsModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Sitecore.Data.Items;
+
+namespace AdvancedImage.Fields.Editor
+{
+    public class AdvancedImageEditorThumbnailsModel
+    {
+        public string ControlId { get; set; }
+        public string ImageSrc { get; set; }
+        public IEnumerable<Item> Thumbnails { get; set; }
+    }
+}
