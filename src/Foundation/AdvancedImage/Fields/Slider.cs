5487934014407b33d97027dc5baaba256ade68b5
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-05 19:27:00 +0100

    Inherited Slider from Input and IContentField

--- src/Foundation/AdvancedImage/Fields/Slider.cs
+++ src/Foundation/AdvancedImage/Fields/Slider.cs
@@ -1,6 +1,18 @@
-﻿namespace AdvancedImage.Fields
+﻿using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Web.UI.HtmlControls;
+
+namespace AdvancedImage.Fields
 {
-    public class Slider
+    public class Slider : Input, IContentField
     {
+        public string GetValue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValue(string value)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
