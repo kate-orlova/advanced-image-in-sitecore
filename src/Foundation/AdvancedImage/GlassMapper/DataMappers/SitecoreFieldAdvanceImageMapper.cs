7600e8a8db8ea89984fb52fc614c3ff51be0a04f
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-11 23:43:40 +0100

    Inherited from AbstractSitecoreFieldMapper

--- src/Foundation/AdvancedImage/GlassMapper/DataMappers/SitecoreFieldAdvanceImageMapper.cs
+++ src/Foundation/AdvancedImage/GlassMapper/DataMappers/SitecoreFieldAdvanceImageMapper.cs
@@ -1,6 +1,19 @@
-﻿namespace AdvancedImage.GlassMapper.DataMappers
+﻿using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.DataMappers;
+
+namespace AdvancedImage.GlassMapper.DataMappers
 {
-    public class SitecoreFieldAdvanceImageMapper
+    public class SitecoreFieldAdvanceImageMapper : AbstractSitecoreFieldMapper
     {
+        public override string SetFieldValue(object value, SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override object GetFieldValue(string fieldValue, SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
