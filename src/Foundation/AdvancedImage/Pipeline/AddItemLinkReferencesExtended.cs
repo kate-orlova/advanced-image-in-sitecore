130c75ea3c70e8b40efa6afb1b681d241460d7b9
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-12 19:49:01 +0100

    Created AddItemLinkReferencesExtended pipeline

---
+++ src/Foundation/AdvancedImage/Pipeline/AddItemLinkReferencesExtended.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Publishing.Pipelines.GetItemReferences;
+using Sitecore.Publishing.Pipelines.PublishItem;
+using System;
+using System.Collections.Generic;
+
+namespace AdvancedImage.Pipeline
+{
+    public class AddItemLinkReferencesExtended : GetItemReferencesProcessor
+    {
+        protected override List<Item> GetItemReferences(PublishItemContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
