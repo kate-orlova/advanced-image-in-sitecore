48b8b6de29532693b75e29a2b7f36f57f0289e1f
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-12 19:30:53 +0100

    Implemented AdvanceImageGalleryField class

---
+++ src/Foundation/AdvancedImage/GlassMapper/Fields/AdvanceImageGalleryField.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedImage.GlassMapper.Fields
+{
+    public class AdvanceImageGalleryField
+    {
+        public IEnumerable<AdvanceImageField> GalleryItems { get; set; }
+    }
+}
