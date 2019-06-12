b78fb4ea371509533fd54a53153bcd01eb0dbba3
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-12 19:32:12 +0100

    Added SitecoreFieldAdvanceImageGalleryMapper class

---
+++ src/Foundation/AdvancedImage/GlassMapper/DataMappers/SitecoreFieldAdvanceImageGalleryMapper.cs
@@ -0,0 +1,19 @@
+﻿using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.DataMappers;
+
+namespace AdvancedImage.GlassMapper.DataMappers
+{
+    public class SitecoreFieldAdvanceImageGalleryMapper : AbstractSitecoreFieldMapper
+    {
+        public override string SetFieldValue(object value, SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override object GetFieldValue(string fieldValue, SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
