80bdaeffd0abcd0a94c920e862235d2751afa796
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-12 19:23:00 +0100

    Created AdvanceImageField class

---
+++ src/Foundation/AdvancedImage/GlassMapper/Fields/AdvanceImageField.cs
@@ -0,0 +1,15 @@
+﻿namespace AdvancedImage.GlassMapper.Fields
+{
+    public class AdvanceImageField : Glass.Mapper.Sc.Fields.Image
+    {
+        public float CropX { get; set; }
+
+        public float CropY { get; set; }
+
+        public float FocusX { get; set; }
+
+        public float FocusY { get; set; }
+
+        public bool ShowFull { get; set; }
+    }
+}
