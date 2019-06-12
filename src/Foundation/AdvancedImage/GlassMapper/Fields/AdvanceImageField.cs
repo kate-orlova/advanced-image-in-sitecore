dfe627ecd185c609897848b6035fe7f2d0e267d1
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-12 19:24:31 +0100

    Implemented GetFocalPointParameters() method

--- src/Foundation/AdvancedImage/GlassMapper/Fields/AdvanceImageField.cs
+++ src/Foundation/AdvancedImage/GlassMapper/Fields/AdvanceImageField.cs
@@ -1,4 +1,6 @@
-﻿namespace AdvancedImage.GlassMapper.Fields
+﻿using System;
+
+namespace AdvancedImage.GlassMapper.Fields
 {
     public class AdvanceImageField : Glass.Mapper.Sc.Fields.Image
     {
@@ -11,5 +13,14 @@
         public float FocusY { get; set; }
 
         public bool ShowFull { get; set; }
+        public string GetFocalPointParameters(int maxWidth = 0, float cropFactor = 0)
+        {
+            if (Math.Abs(cropFactor) < 0.00000001 || maxWidth == 0)
+            {
+                return string.Empty;
+            }
+
+            return $"cx={this.CropX}&cy={this.CropY}&cw={maxWidth}&ch={Math.Ceiling(maxWidth / cropFactor)}";
+        }
     }
 }
