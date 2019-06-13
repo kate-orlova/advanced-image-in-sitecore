cc7b4e8085e031c253d348b0062324aee006729d
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-13 19:11:00 +0100

    Implemented AddMediaItemAndParentsToPublishList() method

--- src/Foundation/AdvancedImage/Pipeline/AddItemLinkReferencesExtended.cs
+++ src/Foundation/AdvancedImage/Pipeline/AddItemLinkReferencesExtended.cs
@@ -3,6 +3,7 @@ using Sitecore.Publishing.Pipelines.GetItemReferences;
 using Sitecore.Publishing.Pipelines.PublishItem;
 using System;
 using System.Collections.Generic;
+using Sitecore;
 
 namespace AdvancedImage.Pipeline
 {
@@ -17,6 +18,19 @@ namespace AdvancedImage.Pipeline
         protected override List<Item> GetItemReferences(PublishItemContext context)
         {
             throw new NotImplementedException();
+        }
+        private void AddMediaItemAndParentsToPublishList(Item target, List<Item> mediaList)
+        {
+            if (target == null || !target.Paths.IsMediaItem)
+                return;
+
+            var parent = target.Parent;
+            while (parent != null && parent.ID != ItemIDs.MediaLibraryRoot)
+            {
+                mediaList.Insert(0, parent);
+                parent = parent.Parent;
+            }
+            mediaList.Add(target);
         }
     }
 }
