3bf1a7728d77bf3c36fdbc2e2ea91012e864e496
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-13 19:14:46 +0100

    Implemented GetAdvanceImageGalleryFieldsLinkedMediaItems() method

--- src/Foundation/AdvancedImage/Pipeline/AddItemLinkReferencesExtended.cs
+++ src/Foundation/AdvancedImage/Pipeline/AddItemLinkReferencesExtended.cs
@@ -3,7 +3,13 @@ using Sitecore.Publishing.Pipelines.GetItemReferences;
 using Sitecore.Publishing.Pipelines.PublishItem;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
 using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Managers;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
 
 namespace AdvancedImage.Pipeline
 {
@@ -18,6 +24,51 @@ namespace AdvancedImage.Pipeline
         protected override List<Item> GetItemReferences(PublishItemContext context)
         {
             throw new NotImplementedException();
+        }
+        protected virtual List<Item> GetAdvanceImageGalleryFieldsLinkedMediaItems(Item item)
+        {
+            var mediaList = new List<Item>();
+
+            var advanceImageGalleryFields = item.Fields
+                .Where(x => string.Equals(x.TypeKey, "advance image gallery", StringComparison.OrdinalIgnoreCase));
+
+            foreach (var advanceImageGalleryField in advanceImageGalleryFields)
+            {
+                try
+                {
+                    var value = advanceImageGalleryField.Value;
+
+                    if (value == null)
+                        continue;
+
+                    var galleryXmlDocument = new XmlDocument
+                    {
+                        InnerXml = value
+                    };
+
+                    var imageValues = galleryXmlDocument.GetElementsByTagName("image");
+
+                    foreach (var imageValue in imageValues)
+                    {
+                        if (!(imageValue is XmlNode xmlNode))
+                            continue;
+
+                        var mediaItemIdAttribute = xmlNode.Attributes?["mediaid"];
+
+                        if (string.IsNullOrWhiteSpace(mediaItemIdAttribute?.Value))
+                            continue;
+
+                        var target = ItemManager.GetItem(new ID(mediaItemIdAttribute.Value), Language.Current, Sitecore.Data.Version.Latest, item.Database);
+                        AddMediaItemAndParentsToPublishList(target, mediaList);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("Error publishing advance image gallery related media items", ex, typeof(AddItemLinkReferencesExtended));
+                }
+            }
+
+            return mediaList;
         }
         private void AddMediaItemAndParentsToPublishList(Item target, List<Item> mediaList)
         {
