192bb46e01eafa05b752c3cee74dafb45be437b5
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-14 18:10:46 +0100

    Created a BaseGlassMapperFluentRegistration class

---
+++ src/Foundation/AdvancedImage/GlassMapper/Models/BaseGlassMapperFluentRegistration.cs
@@ -0,0 +1,9 @@
+﻿using Glass.Mapper.Sc.IoC;
+
+namespace AdvancedImage.GlassMapper.Models
+{
+    public abstract class BaseGlassMapperFluentRegistration
+    {
+        public abstract void RegisterFluently(IDependencyResolver glassDependencyResolver);
+    }
+}
