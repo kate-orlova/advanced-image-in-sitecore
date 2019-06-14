2329db4081b100de5fd3a95bfd22f67f1b6f5b3b
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-14 18:12:44 +0100

    Created a RegisterCustomGlassMapper class

---
+++ src/Foundation/AdvancedImage/Pipeline/RegisterCustomGlassMapper.cs
@@ -0,0 +1,13 @@
+﻿using AdvancedImage.GlassMapper.Models;
+using Glass.Mapper.Sc.IoC;
+
+namespace AdvancedImage.Pipeline
+{
+    public class RegisterCustomGlassMapper : BaseGlassMapperFluentRegistration
+    {
+        public override void RegisterFluently(IDependencyResolver glassDependencyResolver)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
