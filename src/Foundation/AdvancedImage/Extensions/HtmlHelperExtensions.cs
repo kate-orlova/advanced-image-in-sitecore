773bdad889c1ae75a0dd70d4c75760481818515b
Kate Orlova <kate.orlova@deleteagency.com>
2019-06-15 20:59:42 +0100

    Implemented GetSitecoreIconUrl() extension

--- src/Foundation/AdvancedImage/Extensions/HtmlHelperExtensions.cs
+++ src/Foundation/AdvancedImage/Extensions/HtmlHelperExtensions.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using AdvancedImage.Controllers;
+using Sitecore.Data.Managers;
 
 namespace AdvancedImage.Extensions
 {
     public static class HtmlHelperExtensions
     {
+        private static readonly Regex SitecoreImgSrcRegex = new Regex(@"(src\=\""([^""]*)\"")", RegexOptions.Compiled);
+
         public static string GetRazorViewAsString(string viewPath, object model)
         {
             var st = new StringWriter();
@@ -20,6 +24,21 @@ namespace AdvancedImage.Extensions
             var razor = new RazorView(controllerContext, viewPath, null, false, null);
             razor.Render(new ViewContext(controllerContext, razor, new ViewDataDictionary(model), new TempDataDictionary(), st), st);
             return st.ToString();
+        }
+        public static HtmlString GetSitecoreIconUrl(this HtmlHelper helper, string iconPath)
+        {
+            return helper.GetSitecoreIconUrl(iconPath, 16);
+        }
+        public static HtmlString GetSitecoreIconUrl(this HtmlHelper helper, string iconPath, int size)
+        {
+            var imageTag = ThemeManager.GetImage(iconPath, size, size);
+            var match = SitecoreImgSrcRegex.Match(imageTag);
+            if (match.Success)
+            {
+                return new HtmlString(match.Groups[2].Value);
+            }
+
+            return new HtmlString(string.Empty);
         }
     }
 }
