3a869174c41a95f35ca76e956d82299130c0e862
Kate Orlova <kate.orlova@deleteagency.com>
2019-07-01 18:53:17 +0100

    Specified constants

--- src/Foundation/AdvancedImage/Fields/AdvancedImageGallery.cs
+++ src/Foundation/AdvancedImage/Fields/AdvancedImageGallery.cs
@@ -4,6 +4,11 @@ namespace AdvancedImage.Fields
 {
     public class AdvancedImageGallery: LinkBase
     {
+
+        private const string THUMBNAIL_FOLDER_FIELD_NAME = "ThumbnailsFolderID";
+        private const string IMAGES_SOURCE_FOLDER_FIELD_NAME = "ImagesSourceFolderID";
+        private const string IS_DEBUG_FIELD_NAME = "IsDebug";
+        private const string ASSETS_FOLDER_ID = "xxx-yyyy-zzzz";
         public override string GetValue()
         {
             throw new System.NotImplementedException();
