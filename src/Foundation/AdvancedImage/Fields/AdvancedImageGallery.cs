900a158a2aa7892ab1250cc5de2d60714b3b4570
Kate Orlova <kate.orlova@deleteagency.com>
2019-07-01 18:56:04 +0100

    Specified XmlValue property

--- src/Foundation/AdvancedImage/Fields/AdvancedImageGallery.cs
+++ src/Foundation/AdvancedImage/Fields/AdvancedImageGallery.cs
@@ -1,14 +1,35 @@
-﻿using Sitecore.Shell.Applications.ContentEditor;
+﻿using Sitecore.Diagnostics;
+using Sitecore.Shell.Applications.ContentEditor;
 
 namespace AdvancedImage.Fields
 {
-    public class AdvancedImageGallery: LinkBase
+    public class AdvancedImageGallery : LinkBase
     {
-
         private const string THUMBNAIL_FOLDER_FIELD_NAME = "ThumbnailsFolderID";
         private const string IMAGES_SOURCE_FOLDER_FIELD_NAME = "ImagesSourceFolderID";
         private const string IS_DEBUG_FIELD_NAME = "IsDebug";
         private const string ASSETS_FOLDER_ID = "xxx-yyyy-zzzz";
+
+        protected XmlValue XmlValue
+        {
+            get
+            {
+                XmlValue viewStateProperty = this.GetViewStateProperty("XmlValue", null) as XmlValue;
+                if (viewStateProperty == null)
+                {
+                    viewStateProperty = new XmlValue(string.Empty, "gallery");
+                    XmlValue = viewStateProperty;
+                }
+
+                return viewStateProperty;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                SetViewStateProperty("XmlValue", value, null);
+            }
+        }
+
         public override string GetValue()
         {
             throw new System.NotImplementedException();
