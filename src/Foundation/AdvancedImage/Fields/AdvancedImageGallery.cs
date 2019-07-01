412fa40bad10feeeb0b66cd818ee861ba2939d9e
Kate Orlova <kate.orlova@deleteagency.com>
2019-07-01 18:51:47 +0100

    Inherited from LinkBase class

--- src/Foundation/AdvancedImage/Fields/AdvancedImageGallery.cs
+++ src/Foundation/AdvancedImage/Fields/AdvancedImageGallery.cs
@@ -1,6 +1,17 @@
-﻿namespace AdvancedImage.Fields
+﻿using Sitecore.Shell.Applications.ContentEditor;
+
+namespace AdvancedImage.Fields
 {
-    public class AdvancedImageGallery
+    public class AdvancedImageGallery: LinkBase
     {
+        public override string GetValue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetValue(string value)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
