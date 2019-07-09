d56594e6faeee353a26a935c9c952d85c3362921
Kate Orlova <kate.orlova@deleteagency.com>
2019-07-09 18:42:33 +0100

    Implemented string extensions

---
+++ src/Foundation/AdvancedImage/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace AdvancedImage.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool Empty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool ParseOrDefault(this string value, bool defaultValue)
+        {
+            if (value.Empty())
+            {
+                return defaultValue;
+            }
+
+            return bool.TryParse(value, out var parsedValue)
+                ? parsedValue
+                : defaultValue;
+        }
+    }
+}
