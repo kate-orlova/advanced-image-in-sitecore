04880b65e0ff30404b9e621ec01d710934d0bde6
Kate Orlova <kate.orlova@deleteagency.com>
2019-07-10 19:30:27 +0100

    Implemented ToXmlDocument() extension method

--- src/Foundation/AdvancedImage/Extensions/XmlExtensions.cs
+++ src/Foundation/AdvancedImage/Extensions/XmlExtensions.cs
@@ -1,6 +1,18 @@
-﻿namespace AdvancedImage.Extensions
+﻿using System.Xml;
+using System.Xml.Linq;
+
+namespace AdvancedImage.Extensions
 {
     public static class XmlExtensions
     {
+        public static XmlDocument ToXmlDocument(this XDocument xDocument)
+        {
+            var xmlDocument = new XmlDocument();
+            using (var xmlReader = xDocument.CreateReader())
+            {
+                xmlDocument.Load(xmlReader);
+            }
+            return xmlDocument;
+        }
     }
 }
