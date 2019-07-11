e45e489bdb85f6244700f9f4ca28127b5aa842ef
Kate Orlova <kate.orlova@deleteagency.com>
2019-07-11 18:07:44 +0100

    Implemented ToJson() extension method

--- src/Foundation/AdvancedImage/Extensions/ObjectExtensions.cs
+++ src/Foundation/AdvancedImage/Extensions/ObjectExtensions.cs
@@ -1,6 +1,23 @@
-﻿namespace AdvancedImage.Extensions
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace AdvancedImage.Extensions
 {
     public static class ObjectExtensions
     {
+        public static string ToJson(this object source, JsonSerializerSettings jsonSerializerSettings = null, NullValueHandling nullValueHandling = NullValueHandling.Include)
+        {
+            if (jsonSerializerSettings == null)
+            {
+                jsonSerializerSettings = new JsonSerializerSettings
+                {
+                    DateFormatString = "yyyy-MM-ddTHH:mm:ss",
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    NullValueHandling = nullValueHandling
+                };
+            }
+
+            return JsonConvert.SerializeObject(source, jsonSerializerSettings);
+        }
     }
 }
