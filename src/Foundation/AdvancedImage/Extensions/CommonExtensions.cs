031192c9120da8cadca501b75572f103783c3f7f
Kate Orlova <kate.orlova@deleteagency.com>
2019-11-15 19:28:41 +0500

    Implemented ToSchemaOrgJsonString() extension

--- src/Foundation/AdvancedImage/Extensions/CommonExtensions.cs
+++ src/Foundation/AdvancedImage/Extensions/CommonExtensions.cs
@@ -1,6 +1,28 @@
-﻿namespace AdvancedImage.Extensions
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using System;
+
+namespace AdvancedImage.Extensions
 {
     public static class CommonExtensions
     {
+        public static string ToSchemaOrgJsonString<TModel, TSchemaOrgModel>(this TModel model)
+            where TSchemaOrgModel : MXTires.Microdata.Thing
+            where TModel : class
+        {
+            try
+            {
+                var mapper = ServiceLocator.ServiceProvider?.GetService<IMapper>();
+                var schema = mapper?.Map<TModel, TSchemaOrgModel>(model);
+                return schema?.ToJson();
+            }
+            catch (Exception exception)
+            {
+                Sitecore.Diagnostics.Error.LogError($"{exception.Message}\n{exception.StackTrace}");
+            }
+
+            return null;
+        }
     }
 }
