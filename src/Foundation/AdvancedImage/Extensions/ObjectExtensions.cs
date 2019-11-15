1d4b31b5d091af3763360fd9de7f14734586e6ce
Kate Orlova <kate.orlova@deleteagency.com>
2019-11-15 19:34:00 +0500

    Implemented GetHtmlAttributeCollection() extension

--- src/Foundation/AdvancedImage/Extensions/ObjectExtensions.cs
+++ src/Foundation/AdvancedImage/Extensions/ObjectExtensions.cs
@@ -1,11 +1,16 @@
-﻿using Newtonsoft.Json;
+﻿using Glass.Mapper;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
 
 namespace AdvancedImage.Extensions
 {
     public static class ObjectExtensions
     {
-        public static string ToJson(this object source, JsonSerializerSettings jsonSerializerSettings = null, NullValueHandling nullValueHandling = NullValueHandling.Include)
+        public static string ToJson(this object source, JsonSerializerSettings jsonSerializerSettings = null,
+            NullValueHandling nullValueHandling = NullValueHandling.Include)
         {
             if (jsonSerializerSettings == null)
             {
@@ -18,6 +23,37 @@ namespace AdvancedImage.Extensions
             }
 
             return JsonConvert.SerializeObject(source, jsonSerializerSettings);
+        }
+
+        public static NameValueCollection GetHtmlAttributeCollection(this object target, bool lowerCaseName = false,
+            bool underscoreForHyphens = true)
+        {
+            var nameValueCollection = new NameValueCollection();
+            if (target == null) return nameValueCollection;
+
+            foreach (var property in Utilities.GetAllProperties(target.GetType()).OrderBy(p => p.Name == "class"))
+            {
+                var propertyName = lowerCaseName ? property.Name.ToLower() : property.Name;
+                if (underscoreForHyphens)
+                {
+                    propertyName = propertyName.Replace("_", "-");
+                }
+
+                var propertyValue = property.GetValue(target);
+                if (propertyValue == null)
+                {
+                    nameValueCollection.Add(propertyName, string.Empty);
+                    continue;
+                }
+
+                var val = propertyValue.GetType().IsArray
+                    ? string.Join(",", propertyValue as IEnumerable)
+                    : propertyValue.ToString();
+
+                nameValueCollection.Add(propertyName, val);
+            }
+
+            return nameValueCollection;
         }
     }
 }
